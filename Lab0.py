e338777ba1a8dd53941f01b8a4ff161323c6368c
eadsouza <eadsouza@loyola.edu>
2024-09-04 16:18:08 -0400

    I was able to clone the repository, start the file, code the lines about my partner, run and test, and finally committing and pushing this. - Ethan D'Souza

---
+++ Lab0.py
@@ -0,0 +1,7 @@
+print ("CS151 Lab 0")
+print ("Zain")
+print ("Zain likes to collect sneakers.")
+print ("Zain is from Howard County Maryland.")
+print ("Zain is a great hockey player.")
+print ("Zain was born in Virginia.")
+print ("Zain likes to travel to new places.")
